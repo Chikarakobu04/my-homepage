c5ecbbe66b3e2753d80f6e8c63d5274a09a2a557
Chikarakobu04 <san9dai2004@gmail.com>
2024-11-24 18:27:46 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>FC理工</h1>
+        <p>南山大学理工学部生のホームページです。</p>
+        <a href="#about"className="btn">成果物</a>
+      </header>
+      <section id="about">
+        <h2>成果物一覧</h2>
+        <p>これからここに成果物を挙げていきます。</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
